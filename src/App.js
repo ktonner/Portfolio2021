4f47231c25af3769ec58b054d06f042ace350e7e
Kathleen Tonner <kathleentonner@gmail.com>
2020-08-04 12:39:07 -0400

    front page work

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
       <Navigation/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Kathleen Tonner</h1>
+        <p><b>Full Stack Web Developer</b> using a background in content creation to create clean, comprehensive code. Earned a certificate in Full Stack Development from the University of Central Florida, with developed skills in JavaScript, CSS, and Node.js. Applied strong back-end development with MySQL and collaboration skills when working in a team on a recent project to create a book logging application for users to keep track of their favorite books. I am accustomed to working tight deadlines and adapting. Looking forward to contributing to a team’s success and making better digital products. 
+</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,14 +8,15 @@ class Navigation extends React.Component {
 
     render() {
         return (
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }

cc6532d351e6f59cc4639002a7b4ed02dc0b2718
Kathleen Tonner <kathleentonner@gmail.com>
2020-08-04 12:49:10 -0400

    cleaned app.js so home is own component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navbar'
+import Home from './Home'
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Kathleen Tonner</h1>
-        <p><b>Full Stack Web Developer</b> using a background in content creation to create clean, comprehensive code. Earned a certificate in Full Stack Development from the University of Central Florida, with developed skills in JavaScript, CSS, and Node.js. Applied strong back-end development with MySQL and collaboration skills when working in a team on a recent project to create a book logging application for users to keep track of their favorite books. I am accustomed to working tight deadlines and adapting. Looking forward to contributing to a team’s success and making better digital products. 
-</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+const Home = () => (
+    <div>
+        <h1>Kathleen Tonner</h1>
+        <p><b>Full Stack Web Developer</b> using a background in content creation to create clean, comprehensive code. Earned a certificate in Full Stack Development from the University of Central Florida, with developed skills in JavaScript, CSS, and Node.js. Applied strong back-end development with MySQL and collaboration skills when working in a team on a recent project to create a book logging application for users to keep track of their favorite books. I am accustomed to working tight deadlines and adapting. Looking forward to contributing to a team’s success and making better digital products.
+</p>
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+    </div>
+)
+
+export default Home

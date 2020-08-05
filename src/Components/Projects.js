b579051382d0759c6b1f60cb72ad55392f7ee25b
unknown <kathleentonner@gmail.com>
2020-08-05 16:05:23 -0400

    skills now in own component, formating work page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Figure from 'react-bootstrap/Figure'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
 
 
 class Projects extends React.Component {
     render() {
       return (
         <div>
+          <Row>
         <Figure>
         <Figure.Image
           width={400}
           height={500}
+          alt="171x180"
+          src={require('../assets/soapbox.png')}
+        />
+        <Figure.Caption>
+        Social media application that lets you create posts, follow others, or explore hashtags!
+        </Figure.Caption>
+        <Button>Github</Button> 
+        <Button>Deployed</Button>
+      </Figure>
+        <Figure>
+        <Figure.Image
+          width={400}
+          height={200}
           alt="171x180"
           src={require('../assets/booklogger.png')}
         />
@@ -18,40 +33,35 @@ class Projects extends React.Component {
         Register as a user to track your favorite books.
         </Figure.Caption>
         <Button>Github</Button>
+        <Button>Deployed</Button>
       </Figure>
-        <Figure>
+      </Row>
+      <Row>
+      <Figure>
         <Figure.Image
           width={400}
-          height={200}
+          height={100}
           alt="171x180"
-          src={require('../assets/booklogger.png')}
+          src={require('../assets/budget.jpg')}
         />
         <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        Keep track of your budget with a dynamic graph.
         </Figure.Caption>
+        <Button>Github</Button>
+        <Button>Deployed</Button>
       </Figure>
       <Figure>
         <Figure.Image
           width={400}
           height={200}
           alt="171x180"
-          src={require('../assets/booklogger.png')}
+          src={require('../assets/notetaker.png')}
         />
         <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        Write down new notes, edit old ones, or delete them.
         </Figure.Caption>
       </Figure>
-      <Figure>
-        <Figure.Image
-          width={400}
-          height={200}
-          alt="171x180"
-          src={require('../assets/booklogger.png')}
-        />
-        <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
-        </Figure.Caption>
-      </Figure>
+      </Row>
       </div>
       )
     }
--- src/Work/Work.js
+++ src/Work/Work.js
@@ -4,29 +4,15 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import './pcss.css'
 import Projects from '../Components/Projects'
+import Skills from '../Components/Skills'
 
 const Work = () => (
     <Container>
     <Row>
       <Col xs={3} md={3} style={{float:'left'}}>
         <Row>
-    <Col xs={1} md={1} className="skills">
-    <h3>Languages</h3>
-      <p>HTML</p>
-      <p>CSS</p>
-      <p>Javascript</p>
-      <p>Node.js</p>
-      <p>SQL</p>
-    </Col>
-    <Col xs={1} md={1}className="mx-auto skills">
-    <h3>Applications</h3>
-    <p>MySQL</p>
-    <p>MongoDB</p>
-    <p>Express</p>
-    <p>Handlebars</p>
-    <p>React</p>
-    <p>Mongoose</p>
-    </Col>
+      <Skills/>
+   
     </Row>
     </Col>
     <Col xs={9} md={9}className="mx-auto">

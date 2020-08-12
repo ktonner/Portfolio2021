7131f4efc86917e204ab08df380c10e4bf4e9a5e
unknown <kathleentonner@gmail.com>
2020-08-12 17:45:30 -0400

    work links added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,7 +11,7 @@ class Projects extends React.Component {
     render() {
       return (
         <div>
-          <Row>
+          <Row style={{marginBottom:'20px'}}>
         <motion.Figure
         className="mx-auto"
         whileHover={{
@@ -30,8 +30,8 @@ class Projects extends React.Component {
         <Figure.Caption>
         Social media application that lets you create posts, follow others, or explore hashtags!
         </Figure.Caption>
-        <Button>Github</Button> 
-        <Button>Deployed</Button>
+        <Button href="https://github.com/ktonner/SocialMediaApp" target="_blank">Github</Button> 
+        <Button href="https://sleepy-brook-72953.herokuapp.com/public" target="_blank">Deployed</Button>
       </motion.Figure>
       <motion.Figure
       className="mx-auto"
@@ -111,10 +111,31 @@ class Projects extends React.Component {
           width={300}
           height={200}
           alt="171x180"
-          src={require('../assets/weatherdash.png')}
+          src={require('../assets/codingquiz.png')}
         />
         <Figure.Caption>
-        Search for a city and check the weather for today, plus a five-day forecast. Uses third-party API.
+        Test your coding knowledge in a fun quiz game. Earn points the faster you answer correctly and log your high score.
+        </Figure.Caption>
+        <Button href="https://github.com/ktonner/Coding_Quiz" target='_blank'>Github</Button> 
+        <Button href="https://ktonner.github.io/Coding_Quiz/" target='_blank'>Deployed</Button>
+      </motion.Figure>
+      <motion.Figure
+      className="mx-auto"
+        whileHover={{
+          scale: 1.1
+        }}
+        transformTemplate={(props, transform) =>
+          transform.replace(" translateZ(0)", "")
+        }
+        >
+        <Figure.Image
+          width={300}
+          height={200}
+          alt="171x180"
+          src={require('../assets/budget.jpg')}
+        />
+        <Figure.Caption>
+        Keep track of your budget with a dynamic graph.
         </Figure.Caption>
         <Button href="https://github.com/ktonner/Weather_Dash" target='_blank'>Github</Button> 
         <Button href="https://ktonner.github.io/Weather_Dash/" target='_blank'>Deployed</Button>

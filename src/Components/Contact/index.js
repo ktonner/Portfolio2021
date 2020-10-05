fed50e66d6bcc0f38479ed31cf4d6b6df756696d
unknown <kathleentonner@gmail.com>
2020-10-05 17:38:49 -0400

    front page mobile compatible

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -5,11 +5,9 @@ import Row from 'react-bootstrap/Row'
 const Contact = () => {
     return (
         <div>
-            <Row>
-            <h2>Contact</h2>
-            </Row>
+         
             <br></br>
-            <Row>
+         
             {/* <motion.div
                 whileHover={{
                     scale: 1.1,
@@ -31,7 +29,6 @@ const Contact = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z"/></svg>
             </a>
 
-            </Row>
         </div>
     )
 }

bf7e7d7ca16364ab95b8ee92a7b85fe2cdb25ab5
unknown <kathleentonner@gmail.com>
2020-10-14 14:15:51 -0400

    syntax on app.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import logo from './logo.svg';
 import { createBrowserHistory as createHistory } from "history";
 import './App.css';
 import Navigation from './Components/Navbar'
-
 import Home from './pages/Home/Home'
 import Work from './pages/Work/Work'
 import Contact from './Components/Contact/index'
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navigation/>
-      <header className="App-header">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+
+
       <Switch>
-      <Route path="/Portfolio2" exact component={Home} />
-      <Route path="/" exact component={Home} />
-      <Route path="/work" exact component={Work} />
-      <Route path="/Portfolio2/work" exact component={Work} />
+      <Route exact path="/Portfolio2" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/work" component={Work} />
+      <Route path="/Portfolio2/work" component={Work} />
       </Switch>
-      </BrowserRouter>
-      </header>
+     
+     
     </div>
+    </Router>
   );
 }
 

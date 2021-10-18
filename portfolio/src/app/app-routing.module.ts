9b6179f4b1233f2dda57e5084f27f7946d35302d
Kathleen Tonner <KTNN@usny5cg829323l.usny.capco.com>
2021-10-18 16:43:54 -0400

    Basic routing and footer

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ContactComponent } from './contact/contact.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'contact', redirectTo: '/contact', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,18 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomePageComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
